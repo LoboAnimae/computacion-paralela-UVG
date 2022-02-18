04310fe8a4b9cf05072bf44005b1f1a04bf60a8d
LoboAnimae <andyquan12@gmail.com>
2022-02-18 08:59:59 -0600

    Added menu

--- src/main.c
+++ src/main.c
@@ -10,34 +10,70 @@ int main(int argc, char *argv[])
 {
 
     // Create a screen
-    Screen *screen = create_screen();
+    Screen *menu = create_screen();
+    Screen *names = create_screen();
     ScreenComponent *titleComponent = create_screen_component();
     ScreenComponent *bodyComponent = create_screen_component();
+    ScreenComponent *namesComponent = create_screen_component();
     ScreenComponent *footerComponent = create_screen_component();
-    add_component(screen, titleComponent);
-    add_component(screen, bodyComponent);
-    add_component(screen, footerComponent);
+
+    add_component(menu, titleComponent);
+    add_component(menu, bodyComponent);
+    add_component(menu, footerComponent);
+
+    add_component(names, titleComponent);
+    add_component(names, namesComponent);
+    add_component(names, footerComponent);
+
     add_line(titleComponent, "Proyecto 1\n===================\n\n");
-    add_line(bodyComponent, "Body component - ");
-    int toBeUpdated = add_line(bodyComponent, "I will change on the 300th iteration");
-    add_line(footerComponent, "\n\n===================\nThis is a footer component");
-    refresh(screen, NULL);
-    for (int i = 0; i < 1000; i++)
+    add_line(bodyComponent, "\tMenu Principal\n\t_________________\n\n");
+    add_line(bodyComponent, "\tPor favor seleccione una opcion\n");
+    add_line(bodyComponent, "\t\n===================\n\t1. Nombres\n\t2. Salir");
+    add_line(namesComponent, "\tNombres\n\t_________________\n\n");
+    add_line(namesComponent, "\tNombre\tApellido\t17652\n");
+    add_line(namesComponent, "\tAndrés\tQuan-Littow\tCarnet\n");
+    add_line(namesComponent, "\tPablo\tRuiz\t\tCarnet\n");
+    add_line(namesComponent, "\tMichael\tChan\t\tCarnet\n");
+    add_line(namesComponent, "\n\n===================\n\t1. Atrás");
+
+    add_line(footerComponent, "\n\n====================\n>>> ");
+    refresh(menu, NULL);
+
+    Screen *currentScreen = menu;
+    refresh(currentScreen, NULL);
+    int currentScreenIndex = 0;
+    while (1)
     {
-        printf("\n\n\nDebug: Current Iteration->%d\n", i);
-        if (i == 250)
+        int option;
+        scanf("%d", &option);
+        if (currentScreenIndex == 0)
         {
-            toBeUpdated = update_line(bodyComponent, toBeUpdated, "I changed!", NULL);
+            if (option == 1)
+            {
+                currentScreen = names;
+                currentScreenIndex = 1;
+                refresh(currentScreen, NULL);
+            }
+            else if (option == 2)
+            {
+                break;
+            }
         }
-        else if (i == 400)
+        else if (currentScreenIndex == 1)
         {
-            toBeUpdated = update_line(bodyComponent, toBeUpdated, "I changed again!", NULL);
+            if (option == 1)
+            {
+                currentScreen = menu;
+                currentScreenIndex = 0;
+                refresh(currentScreen, NULL);
+            }
         }
-        else if (i == 600)
+        else
         {
-            remove_line(bodyComponent, toBeUpdated, NULL);
+            printf("[ERROR] [BUG] THE SCREEN INDEX IS OUT OF RANGE\n");
+            return -1;
         }
-        refresh(screen, NULL);
     }
+
     return 0;
 }
