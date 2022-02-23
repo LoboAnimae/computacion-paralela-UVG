9edaefe4a0fec988b8ad74b7c9efe372064ae1a1
pablo-fl <59905458+PingMaster99@users.noreply.github.com>
2022-02-22 21:20:22 -0600

    Create fibonacci.c

---
+++ Ejercicio_i/fibonacci.c
@@ -0,0 +1,16 @@
+//
+// Created by XaolinVikings on 22/02/2022 Twosday.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+void Usage(char *prog_name);
+
+int main(int argc, char *argv[])
+{
+
+} /* main */
+
