da591660542fd9cfcdeb167e762d0c58966e99eb
Yagdrassyl <qua17652@uvg.edu.gt>
2022-03-04 23:45:48 -0600

    Added Abstractions

---
+++ include/math/Bar.h
@@ -0,0 +1,52 @@
+#ifndef BAR_PROJECT_H
+#define BAR_PROJECT_H
+
+typedef struct Deltas
+{
+    double delta_t;
+    double delta_x;
+} Deltas;
+
+typedef struct Times
+{
+    long double t_j;
+    long double t_adjacent_1;
+    long double t_adjacent_2;
+} Times;
+
+typedef struct Sequence
+{
+    long double t_0;
+    long double t_left;
+    long double t_right;
+} Sequence;
+
+typedef struct Params
+{
+    long double err;
+    long int n;
+    double c;
+    double l;
+} Params;
+
+/**
+ * @brief Calculates the temperatures of a metal bar
+ *
+ * @param seq Allows for the left and right temps to be known
+ * @param param Allows for the misc params to be passed
+ * @param delta Allows for the deltas to be known
+ * @return long double*
+ */
+long double *caulculate_temperatures(Sequence *seq, Params *param, Deltas *delta);
+
+/**
+ * @brief Gets the new temperature
+ *
+ * @param time The current time struct
+ * @param delta a Delta struct
+ * @param param a Params struct
+ * @return long double
+ */
+long double obtain_temperature(Times *time, Deltas *delta, Params *param);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -6,9 +6,13 @@
 
 #include <stdlib.h>
 
+#define SEPARATOR "\n=====================================\n"
+#define SUBSEPARATOR "\n____________________________________\n"
+
 int main(int argc, char *argv[])
 {
 
+    int file_loaded_message;
     // Create a screen
     Screen *menu = create_screen();
     Screen *names = create_screen();
@@ -27,16 +31,22 @@ int main(int argc, char *argv[])
     add_component(names, namesComponent);
     add_component(names, footerComponent);
 
-    add_line(titleComponent, "Proyecto 1\n===================\n\n");
-    add_line(bodyComponent, "\tMenu Principal\n\t_________________\n\n");
-    add_line(bodyComponent, "\tPor favor seleccione una opcion\n");
-    add_line(bodyComponent, "\t\n===================\n\t1. Nombres\n\t2. Salir");
+    add_line(titleComponent, "Proyecto 1");
+    add_line(titleComponent, SEPARATOR);
+    add_line(fileComponent, "Archivo: ");
+    file_loaded_message = add_line(fileComponent, "NO CARGADO\n");
+    add_line(fileComponent, SEPARATOR);
+    add_line(bodyComponent, "\tMenu Principal\t");
+    add_line(bodyComponent, SUBSEPARATOR);
+    add_line(bodyComponent, "\n\tPor favor seleccione una opcion\n");
+    add_line(bodyComponent, "\t1. Nombres\n\t2. Salir");
     add_line(namesComponent, "\tNombres\n\t_________________\n\n");
-    add_line(namesComponent, "\tNombre\tApellido\t17652\n");
-    add_line(namesComponent, "\tAndrés\tQuan-Littow\tCarnet\n");
+    add_line(namesComponent, "\tNombre\tApellido\tCarnet\n");
+    add_line(namesComponent, "\tAndrés\tQuan-Littow\t17652\n");
     add_line(namesComponent, "\tPablo\tRuiz\t\tCarnet\n");
     add_line(namesComponent, "\tMichael\tChan\t\tCarnet\n");
-    add_line(namesComponent, "\n\n===================\n\t1. Atrás");
+    add_line(namesComponent, SEPARATOR);
+    add_line(namesComponent, "\t1. Atrás");
 
     add_line(footerComponent, "\n\n====================\n>>> ");
     refresh(menu, NULL);
@@ -78,40 +88,4 @@ int main(int argc, char *argv[])
     }
 
     return 0;
-}
-
-/**
- * Calculates the temperatures of a metal bar
- * **/
-long double * calculate_temperatures(long double err, long int n, long double t_0, long double t_left, long double t_right double l, double c) {
-    int remaining_iterations = 10000000000;
-    long double current_error;                       // Check to see if we stop the loop
-    long double new_temperature;                     // New calculated temperature
-    long double temperatures[n];                     // All temperatures
-    temperatures[0] = t_left;
-    temperatures[1] = t_0;
-    temperatures[2] = t_0;
-    temperatures[n-1] = t_right;
-
-    while (current_error > err || remaining_iterations == 0) {
-        for (int j = 1; j < n; j++) {
-            // Computes the new temperature, adds it to the error calculation, and updates temperature list
-            new_temperature = obtain_temperature(temperatures[j], c, (0.1 * pow(delta_x, 2)) / c, l / n, temperatures[j-1], temperatures[j+1]);
-            current_error += fabsl(temperatures[j] - new_temperature);
-            temperatures[j] = new_temperature;
-        }
-        current_error /= n;
-        remaining_iterations--;
-    }
-    return temperatures;
-}
-
-
-
-/**
- * Gets the next temperature
- * **/
-long double obtain_temperature(long double t_j, double c, double delta_t, double delta_x, long double t_adjacent_1, long double t_adjacent_2) {
-    // Returns the new temperature
-    return t_j + ((c * delta_t) / (pow(delta_x, 2))) * (t_adjacent_1 - 2 * t_j + t_adjacent_2);
 }
