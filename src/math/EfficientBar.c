d81461583223e2b7428abd48d36e6bf458c5d428
pablo-fl <59905458+PingMaster99@users.noreply.github.com>
2022-03-19 02:03:26 -0600

    ADD: More efficient bar, parallel testing without swaps

--- src/math/EfficientBar.c
+++ src/math/EfficientBar.c
@@ -1,16 +1,30 @@
 #include "math/Bar.h"
 #include "math.h"
 #include <stdlib.h>
-
+#include <omp.h>
 #define DEFAULT_REMAINING_ITERATIONS 10000000000
+
+
+/**
+ * Discussed options for parallelization:
+ * Producer + consumer (we use two consumers and avoid frontier value exchange)
+ * Tasks (as in pseudocode)
+ * Sections
+ * **/
+
+long double obtain_temperature(Times *time, Deltas *delta, Params *param)
+{
+    return time->t_j * (1 + (param->c * delta->delta_t / delta->delta_x) * (time->t_adjacent_1 - 2 + time->t_adjacent_2));
+}
 
 long double *calculate_temperatures(Sequence *seq, Params *param)
 {
+    omp_set_num_threads(6) // Threads to test with
     long long ll_remaining_iterations = DEFAULT_REMAINING_ITERATIONS;
-    long double ld_current_error, // Check to see if we stop the loop
-    ld_new_temperature,       // New calculated temperature
-    *ldp_temperatures;        // All temperatures
-    *delta;
+    long double ld_current_error = 0, // Check to see if we stop the loop
+        ld_new_temperature,       // New calculated temperature
+        *ldp_temperatures,        // All temperatures
+        *delta;
 
 #pragma region Heap Memory
     ldp_temperatures = (long double *)malloc(sizeof(long double) * param->n);
@@ -23,7 +37,7 @@ long double *calculate_temperatures(Sequence *seq, Params *param)
     ldp_temperatures[0] = seq->t_left;              // TL
     ldp_temperatures[param->n - 1] = seq->t_right;  // TR
     delta->delta_x = 1 / param->n;
-    delta->delta_t = 0.3 * delta->delta_x * delta->delta_x / param-> c
+    delta->delta_t = 0.3 * delta->delta_x * delta->delta_x / param-> c;
 
     while (ld_current_error > param->err && ll_remaining_iterations-- > 0)
     {
@@ -35,16 +49,10 @@ long double *calculate_temperatures(Sequence *seq, Params *param)
                     .t_adjacent_2 = ldp_temperatures[j + 1]
             };
             ld_new_temperature = obtain_temperature(&time, delta, param);
-            ld_current_error += fabsl(ldp_temperatures[j] - ld_new_temperature);
+            ld_current_error = (ld_current_error < fabsl(ldp_temperatures[j] - ld_new_temperature)) ? fabsl(ldp_temperatures[j] - ld_new_temperature) : ld_current_error;
             ldp_temperatures[j] = ld_new_temperature;
         }
-        ld_current_error /= param->n;
     }
 
     return ldp_temperatures;
-}
-
-long double obtain_temperature(Times *time, Deltas *delta, Params *param)
-{
-    return time->t_j + (param->c * delta->delta_t / delta->delta_x) * (time->t_adjacent_1 - 2 * time->t_j + time->t_adjacent_2);
 }
